e653ec5062f3559b2ee191bfbc79c964fab24b45
crisair <renteriariaisabelcristina@gmail.com>
2023-09-01 22:21:41 -0400

    Entrega

--- src/vehiculo/Main.java
+++ src/vehiculo/Main.java
@@ -12,12 +12,17 @@ public class Main {
     public static void main(String[] args) throws Exception {
         
         Motocicleta moto1 = new Motocicleta("maria",2019,4000000,50,1,3);
-        Automovil carro1 = new Automovil("maria",2018,78000000,100,80,3);
+        Automovil carro1 = new Automovil("maria",2018,78000000,100,80,2);
         
        
-        carro1.VaciarEstanqueo(3);
         moto1.ImprimirDatos();
+        moto1.CargarEstanque(12);
         moto1.VaciarEstanqueo(1);
+        System.out.println("*****************");
+        carro1.ImprimirDatos();
+        carro1.VaciarEstanqueo(3);
+        carro1.CargarEstanque(34);
+        carro1.calcularImpuesto();
 }
     
 }
